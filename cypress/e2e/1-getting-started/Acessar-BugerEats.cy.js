8e11833729390e35cdeb4375423b9ea6669851c2
Leonardo Soares <leonardofps@gmail.com>
2022-11-08 12:47:10 -0300

    Automação Buger Eats

---
+++ cypress/e2e/1-getting-started/Acessar-BugerEats.cy.js
@@ -0,0 +1,15 @@
+/// <reference types='cypress' />
+
+describe('Automação Buger Eats - Cypress',() => {
+    beforeEach(() => {
+        cy.visit('https://buger-eats.vercel.app/')
+    })
+
+    it('Acessar home e validar title', () => {
+        cy.title().should('be.equal', 'Buger Eats')
+        cy.get('h1').should('have.text','Seja um parceiro entregador pela Buger Eats')
+    })
+
+    
+
+})
