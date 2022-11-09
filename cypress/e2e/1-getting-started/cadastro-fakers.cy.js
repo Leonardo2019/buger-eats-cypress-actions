9ef40aaa08606ca48fe0f1491a339321e136e01a
Leonardo Soares <leonardofps@gmail.com>
2022-11-09 13:56:24 -0300

    Adicionado script campos obrigatório

--- cypress/e2e/1-getting-started/cadastro-fakers.cy.js
+++ cypress/e2e/1-getting-started/cadastro-fakers.cy.js
@@ -1,5 +1,7 @@
 /// <reference types='cypress' />
 
+import {faker} from '@faker-js/faker';
+
 // import {slowCypressDown} from 'cypress-slow-down'
 // slowCypressDown(600)
 
@@ -15,16 +17,98 @@ describe('Cadastro Fakers - Burger Eats',() => {
     })
 
     it('Cadastro fakers - Método Moto', () => {
-        cy.MetodoMoto()
+        var fakers = require('faker-br');
+        let CpfRandom = fakers.br.cpf();        
+
+        cy.get('strong').should('have.text', 'Cadastre-se para fazer entregas').click()
+        cy.get('input[name="name"]').type(faker.internet.userName())
+        cy.get('input[name="cpf"]').type(CpfRandom).should('be.visible')
+        cy.get('input[name="email"]').type(faker.internet.email()).should('be.visible')
+        cy.get('input[name="whatsapp"]').type(faker.phone.number('9########'))
+        cy.get('input[name="postalcode"]').type('02235000').should('be.visible')
+        cy.get('input[type="button"]').click().should('be.visible')
+        cy.get('input[name="address"]').should('be.visible')
+        cy.get('input[name="address-number"]').type(faker.phone.number('###'))
+        cy.get('input[name="address-details"]').type(faker.vehicle.vehicle())
+        cy.get('input[name="district"]').should('be.visible')
+        cy.get('input[name="city-uf"]').should('be.visible')
+        cy.contains('Moto').click()
+
+        cy.get('input[accept^="image"]').attachFile('/Images/' + 'CNH.jpg')
+        cy.get('.button-success').should('be.visible').click()
+        cy.get('.swal2-popup').should('be.visible')
+        cy.get('.swal2-confirm').click()
+
 
     })
 
     it('Entregador método - Bicicleta', () => {
-        cy.MetodoBicicleta()
+        var fakers = require('faker-br');
+        let CpfRandom = fakers.br.cpf();        
+
+        cy.get('strong').should('have.text', 'Cadastre-se para fazer entregas').click()
+        cy.get('input[name="name"]').type(faker.internet.userName())
+        cy.get('input[name="cpf"]').type(CpfRandom).should('be.visible')
+        cy.get('input[name="email"]').type(faker.internet.email()).should('be.visible')
+        cy.get('input[name="whatsapp"]').type(faker.phone.number('9########'))
+        cy.get('input[name="postalcode"]').type('02235000').should('be.visible')
+        cy.get('input[type="button"]').click().should('be.visible')
+        cy.get('input[name="address"]').should('be.visible')
+        cy.get('input[name="address-number"]').type(faker.phone.number('###'))
+        cy.get('input[name="address-details"]').type(faker.vehicle.vehicle())
+        cy.get('input[name="district"]').should('be.visible')
+        cy.get('input[name="city-uf"]').should('be.visible')
+        cy.contains('Bicicleta').click()
+
+        cy.get('input[accept^="image"]').attachFile('/Images/' + 'CNH.jpg')
+        cy.get('.button-success').should('be.visible').click()
+        cy.get('.swal2-popup').should('be.visible')
+        cy.get('.swal2-confirm').click()
     })
 
-    it('Entregador método - Van/Carro', () => {
-        cy.MetodoVanCarro()
+        it('Entregador método - Van/Carro', () => {
+        var fakers = require('faker-br');
+        let CpfRandom = fakers.br.cpf();        
+
+        cy.get('strong').should('have.text', 'Cadastre-se para fazer entregas').click()
+        cy.get('input[name="name"]').type(faker.internet.userName())
+        cy.get('input[name="cpf"]').type(CpfRandom).should('be.visible')
+        cy.get('input[name="email"]').type(faker.internet.email()).should('be.visible')
+        cy.get('input[name="whatsapp"]').type(faker.phone.number('9########'))
+        cy.get('input[name="postalcode"]').type('02235000').should('be.visible')
+        cy.get('input[type="button"]').click().should('be.visible')
+        cy.get('input[name="address"]').should('be.visible')
+        cy.get('input[name="address-number"]').type(faker.phone.number('###'))
+        cy.get('input[name="address-details"]').type(faker.vehicle.vehicle())
+        cy.get('input[name="district"]').should('be.visible')
+        cy.get('input[name="city-uf"]').should('be.visible')
+        cy.contains('Van/Carro').click()
+
+        cy.get('input[accept^="image"]').attachFile('/Images/' + 'CNH.jpg')
+        cy.get('.button-success').should('be.visible').click()
+        cy.get('.swal2-popup').should('be.visible')
+        cy.get('.swal2-confirm').click()
     })
+
+    it('Mensagens dos campos obrigatórios', () => {
+        cy.get('strong').should('have.text', 'Cadastre-se para fazer entregas').click()
+        cy.get('.button-success').should('be.visible').click()
+        
+        cy.get('input[name="name"]').should('be.empty')
+        cy.contains('É necessário informar o nome').should('be.visible')
+        cy.get('input[name="cpf"]').should('be.empty')
+        cy.contains('É necessário informar o CPF').should('be.visible')
+        cy.get('input[name="email"]').should('be.empty')
+        cy.contains('É necessário informar o email').should('be.visible')
+        cy.get('input[name="postalcode"]').should('be.empty')
+        cy.contains('É necessário informar o CEP').should('be.visible')
+        cy.get('input[name="address-number"]').should('be.empty')
+        cy.contains('É necessário informar o número do endereço').should('be.visible')
+
+        cy.contains('Selecione o método de entrega').should('be.visible')
+        cy.get('input[accept^="image"]').should('be.empty')
+        cy.contains('Adicione uma foto da sua CNH').should('be.visible')
+
+    } )
     
 })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,76 +1,76 @@
 
-import {faker} from '@faker-js/faker';
+// import {faker} from '@faker-js/faker';
 
-Cypress.Commands.add('MetodoMoto', () => {
-        var fakers = require('faker-br');
-        let CpfRandom = fakers.br.cpf();        
+// Cypress.Commands.add('MetodoMoto', () => {
+//         var fakers = require('faker-br');
+//         let CpfRandom = fakers.br.cpf();        
 
-        cy.get('strong').should('have.text', 'Cadastre-se para fazer entregas').click()
-        cy.get('input[name="name"]').type(faker.internet.userName())
-        cy.get('input[name="cpf"]').type(CpfRandom).should('be.visible')
-        cy.get('input[name="email"]').type(faker.internet.email()).should('be.visible')
-        cy.get('input[name="whatsapp"]').type(faker.phone.number('9########'))
-        cy.get('input[name="postalcode"]').type('02235000').should('be.visible')
-        cy.get('input[type="button"]').click().should('be.visible')
-        cy.get('input[name="address"]').should('be.visible')
-        cy.get('input[name="address-number"]').type(faker.phone.number('###'))
-        cy.get('input[name="address-details"]').type(faker.vehicle.vehicle())
-        cy.get('input[name="district"]').should('be.visible')
-        cy.get('input[name="city-uf"]').should('be.visible')
-        cy.contains('Moto').click()
+//         cy.get('strong').should('have.text', 'Cadastre-se para fazer entregas').click()
+//         cy.get('input[name="name"]').type(faker.internet.userName())
+//         cy.get('input[name="cpf"]').type(CpfRandom).should('be.visible')
+//         cy.get('input[name="email"]').type(faker.internet.email()).should('be.visible')
+//         cy.get('input[name="whatsapp"]').type(faker.phone.number('9########'))
+//         cy.get('input[name="postalcode"]').type('02235000').should('be.visible')
+//         cy.get('input[type="button"]').click().should('be.visible')
+//         cy.get('input[name="address"]').should('be.visible')
+//         cy.get('input[name="address-number"]').type(faker.phone.number('###'))
+//         cy.get('input[name="address-details"]').type(faker.vehicle.vehicle())
+//         cy.get('input[name="district"]').should('be.visible')
+//         cy.get('input[name="city-uf"]').should('be.visible')
+//         cy.contains('Moto').click()
 
-        cy.get('input[accept^="image"]').attachFile('/Images/' + 'CNH.jpg')
-        cy.get('.button-success').should('be.visible').click()
-        cy.get('.swal2-popup').should('be.visible')
-        cy.get('.swal2-confirm').click()
+//         cy.get('input[accept^="image"]').attachFile('/Images/' + 'CNH.jpg')
+//         cy.get('.button-success').should('be.visible').click()
+//         cy.get('.swal2-popup').should('be.visible')
+//         cy.get('.swal2-confirm').click()
 
-}),
+// }),
 
-Cypress.Commands.add('MetodoBicicleta', () => {
-    var fakers = require('faker-br');
-    let CpfRandom = fakers.br.cpf();        
+// Cypress.Commands.add('MetodoBicicleta', () => {
+//     var fakers = require('faker-br');
+//     let CpfRandom = fakers.br.cpf();        
 
-    cy.get('strong').should('have.text', 'Cadastre-se para fazer entregas').click()
-    cy.get('input[name="name"]').type(faker.internet.userName())
-    cy.get('input[name="cpf"]').type(CpfRandom).should('be.visible')
-    cy.get('input[name="email"]').type(faker.internet.email()).should('be.visible')
-    cy.get('input[name="whatsapp"]').type(faker.phone.number('9########'))
-    cy.get('input[name="postalcode"]').type('02235000').should('be.visible')
-    cy.get('input[type="button"]').click().should('be.visible')
-    cy.get('input[name="address"]').should('be.visible')
-    cy.get('input[name="address-number"]').type(faker.phone.number('###'))
-    cy.get('input[name="address-details"]').type(faker.vehicle.vehicle())
-    cy.get('input[name="district"]').should('be.visible')
-    cy.get('input[name="city-uf"]').should('be.visible')
-    cy.contains('Bicicleta').click()
+//     cy.get('strong').should('have.text', 'Cadastre-se para fazer entregas').click()
+//     cy.get('input[name="name"]').type(faker.internet.userName())
+//     cy.get('input[name="cpf"]').type(CpfRandom).should('be.visible')
+//     cy.get('input[name="email"]').type(faker.internet.email()).should('be.visible')
+//     cy.get('input[name="whatsapp"]').type(faker.phone.number('9########'))
+//     cy.get('input[name="postalcode"]').type('02235000').should('be.visible')
+//     cy.get('input[type="button"]').click().should('be.visible')
+//     cy.get('input[name="address"]').should('be.visible')
+//     cy.get('input[name="address-number"]').type(faker.phone.number('###'))
+//     cy.get('input[name="address-details"]').type(faker.vehicle.vehicle())
+//     cy.get('input[name="district"]').should('be.visible')
+//     cy.get('input[name="city-uf"]').should('be.visible')
+//     cy.contains('Bicicleta').click()
 
-    cy.get('input[accept^="image"]').attachFile('/Images/' + 'CNH.jpg')
-    cy.get('.button-success').should('be.visible').click()
-    cy.get('.swal2-popup').should('be.visible')
-    cy.get('.swal2-confirm').click()
-}),
+//     cy.get('input[accept^="image"]').attachFile('/Images/' + 'CNH.jpg')
+//     cy.get('.button-success').should('be.visible').click()
+//     cy.get('.swal2-popup').should('be.visible')
+//     cy.get('.swal2-confirm').click()
+// }),
 
-Cypress.Commands.add('MetodoVanCarro', () => {
-    var fakers = require('faker-br');
-    let CpfRandom = fakers.br.cpf();        
+// Cypress.Commands.add('MetodoVanCarro', () => {
+//     var fakers = require('faker-br');
+//     let CpfRandom = fakers.br.cpf();        
 
-    cy.get('strong').should('have.text', 'Cadastre-se para fazer entregas').click()
-    cy.get('input[name="name"]').type(faker.internet.userName())
-    cy.get('input[name="cpf"]').type(CpfRandom).should('be.visible')
-    cy.get('input[name="email"]').type(faker.internet.email()).should('be.visible')
-    cy.get('input[name="whatsapp"]').type(faker.phone.number('9########'))
-    cy.get('input[name="postalcode"]').type('02235000').should('be.visible')
-    cy.get('input[type="button"]').click().should('be.visible')
-    cy.get('input[name="address"]').should('be.visible')
-    cy.get('input[name="address-number"]').type(faker.phone.number('###'))
-    cy.get('input[name="address-details"]').type(faker.vehicle.vehicle())
-    cy.get('input[name="district"]').should('be.visible')
-    cy.get('input[name="city-uf"]').should('be.visible')
-    cy.contains('Van/Carro').click()
+//     cy.get('strong').should('have.text', 'Cadastre-se para fazer entregas').click()
+//     cy.get('input[name="name"]').type(faker.internet.userName())
+//     cy.get('input[name="cpf"]').type(CpfRandom).should('be.visible')
+//     cy.get('input[name="email"]').type(faker.internet.email()).should('be.visible')
+//     cy.get('input[name="whatsapp"]').type(faker.phone.number('9########'))
+//     cy.get('input[name="postalcode"]').type('02235000').should('be.visible')
+//     cy.get('input[type="button"]').click().should('be.visible')
+//     cy.get('input[name="address"]').should('be.visible')
+//     cy.get('input[name="address-number"]').type(faker.phone.number('###'))
+//     cy.get('input[name="address-details"]').type(faker.vehicle.vehicle())
+//     cy.get('input[name="district"]').should('be.visible')
+//     cy.get('input[name="city-uf"]').should('be.visible')
+//     cy.contains('Van/Carro').click()
 
-    cy.get('input[accept^="image"]').attachFile('/Images/' + 'CNH.jpg')
-    cy.get('.button-success').should('be.visible').click()
-    cy.get('.swal2-popup').should('be.visible')
-    cy.get('.swal2-confirm').click()
-})
+//     cy.get('input[accept^="image"]').attachFile('/Images/' + 'CNH.jpg')
+//     cy.get('.button-success').should('be.visible').click()
+//     cy.get('.swal2-popup').should('be.visible')
+//     cy.get('.swal2-confirm').click()
+// })
 
